85a5b6a08bfbeeea5f08309940a913fe651f5f85
Peter Xu <peterx@redhat.com>
2021-03-25 19:12:33 -0400

    rt-trace: support 64 cores cpumask
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- rt-trace-bcc.py
+++ rt-trace-bcc.py
@@ -16,7 +16,6 @@
 #
 # Hooks to observe on isolcpus:
 # - kprobes
-#   - irq_work_queue (queue work locally)
 #   - smp_apic_timer_interrupt/__sysvec_apic_timer_interrupt
 # - tracepoints
 #   - sched_switch
@@ -27,12 +26,11 @@
 # - queue_work_on
 # - smp_call_function
 # - smp_call_function_any
-# - smp_call_function_many
-# - smp_call_function_many_cond
-# - smp_call_function_single
-# - smp_call_function_single_async
-# - irq_work_queue_on
-# - on_each_cpu_cond_mask
+# - smp_call_function_many_cond (covers on_each_cpu_cond_mask, smp_call_function_many)
+# - generic_exec_single (covers smp_call_function_single, smp_call_function_single_async)
+#
+# TODO:
+# - Support MAX_N_CPUS to 256 cores, maybe?  Currently it's 64.
 
 from bcc import BPF
 import argparse
@@ -88,7 +86,7 @@ kprobe_list = {
         "subtype": KPROBE_T_TRACE_LOCAL,
     },
     "smp_call_function_many_cond": {
-        # Includes "smp_call_function_many"
+        # Includes "smp_call_function_many", "on_each_cpu_cond_mask"
         "enabled": True,
         "subtype": KPROBE_T_FIRST_CPUMASK_CONTAINS,
     },
@@ -96,14 +94,6 @@ kprobe_list = {
         # Includes "smp_call_function_single", "smp_call_function_single_async"
         "enabled": True,
         "subtype": KPROBE_T_FIRST_INT_MATCH,
-    },
-    "irq_work_queue_on": {
-        "enabled": False,
-        "subtype": KPROBE_T_TRACE_LOCAL,
-    },
-    "on_each_cpu_cond_mask": {
-        "enabled": False,
-        "subtype": KPROBE_T_TRACE_LOCAL,
     },
     "apic_timer_interrupt": {
         "enabled": False,
@@ -127,13 +117,19 @@ cpu_list = []
 bpf = None
 stack_traces = None
 args = None
-MAX_N_CPUS = 1024
+# Limit this because we use one u64 as cpumask.  Problem is BPF does not allow
+# loop, so any real cpumask won't work.
+MAX_N_CPUS = 64
+
+def err(out):
+    print("ERROR: " + out)
+    exit(-1)
 
 def parse_cpu_list(cpu_list):
     out = []
     def check_index(n):
         if n >= MAX_N_CPUS:
-            err("CPU index overflow (%s>%s)" % (n, MAX_N_CPUS))
+            err("CPU index overflow (%s>=%s)" % (n, MAX_N_CPUS))
     subsets = cpu_list.split(",")
     for subset in subsets:
         if "-" in subset:
@@ -179,15 +175,21 @@ struct data_t {
     char comm[TASK_COMM_LEN];
     u32 msg_type;
     u32 pid;
-    u32 stack_id;
     u32 cpu;
+#if BACKTRACE_ENABLED
+    u32 stack_id;
+#endif
 };
+
 // Used to communicate with the userspace program
 BPF_PERF_OUTPUT(events);
+// Cpumask of which trace is enabled.  Now only covers 64 cores.
+BPF_ARRAY(trace_enabled, u64, 1);
+
+#if BACKTRACE_ENABLED
 // Calltrace buffers
 BPF_STACK_TRACE(stack_traces, 1024);
-// Whether trace enabled on this cpu
-BPF_ARRAY(trace_enabled, u64, %s);
+#endif
 
 // Base function to be called by all kinds of hooks
 static inline void
@@ -200,37 +202,46 @@ kprobe_common(struct pt_regs *ctx, u32 msg_type)
     data.pid = bpf_get_current_pid_tgid();
     data.ts = bpf_ktime_get_ns();
     data.cpu = cpu;
-    if (%d) {
-        // stack_id can be -EFAULT (0xfffffff2) when not applicable
-        data.stack_id = stack_traces.get_stackid(ctx, 0);
-    }
+#if BACKTRACE_ENABLED
+    // stack_id can be -EFAULT (0xfffffff2) when not applicable
+    data.stack_id = stack_traces.get_stackid(ctx, 0);
+#endif
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
     events.perf_submit(ctx, &data, sizeof(data));
+}
+
+static inline u64* get_cpu_list(void)
+{
+    int index = 0;
+
+    return trace_enabled.lookup(&index);
 }
 
 // Submit message as long as the core has enabled tracing
 static inline void
 kprobe_trace_local(struct pt_regs *ctx, u32 msg_type)
 {
-    u32 cpu = bpf_get_smp_processor_id();
-    u64 *enabled = trace_enabled.lookup(&cpu);
+    int cpu = bpf_get_smp_processor_id();
+    u64 *cpu_list = get_cpu_list();
 
-    if (!enabled || !*enabled)
+    if (cpu >= MAX_N_CPUS || !cpu_list)
         return;
 
-    kprobe_common(ctx, msg_type);
+    if ((1UL << cpu) & *cpu_list)
+        kprobe_common(ctx, msg_type);
 }
 
 // Submit only if the specified cpu is in the tracing cpu list
 static inline void
 kprobe_first_arg_match(struct pt_regs *ctx, int cpu, u32 msg_type)
 {
-    u64 *enabled = trace_enabled.lookup(&cpu);
+    u64 *cpu_list = get_cpu_list();
     
-    if (!enabled || !*enabled)
+    if (cpu >= MAX_N_CPUS || !cpu_list)
         return;
 
-    kprobe_common(ctx, msg_type);
+    if ((1UL << cpu) & *cpu_list)
+        kprobe_common(ctx, msg_type);
 }
 
 // Submit only if any specified cpu is in the cpumask
@@ -238,35 +249,22 @@ static inline void
 kprobe_first_cpumask_contains(struct pt_regs *ctx, struct cpumask *mask,
                               u32 msg_type)
 {
-    int cpu, trace_this = 0;
-    u64 *enabled;
+    u64 *cpu_list = get_cpu_list(), *ptr = (u64 *)mask;
 
-    // TODO: This may be less efficient than a cpumask intersection, but we
-    // need to know how to interface a cpumask between Python and bpf..
-    for_each_cpu(cpu, mask) {
-        enabled = trace_enabled.lookup(&cpu);
-        if (enabled && *enabled) {
-            trace_this = 1;
-            break;
-        }
-    }
+    if (!cpu_list || !ptr)
+        return;
 
-    if (trace_this) {
+    if (*cpu_list & *ptr)
         kprobe_common(ctx, msg_type);
-    }
 }
 
 GENERATED_HOOKS
-""" % (MAX_N_CPUS, args.backtrace)
+"""
 
 # Allow quitting the tracing using Ctrl-C
 def int_handler(signum, frame):
     exit(0)
 signal.signal(signal.SIGINT, int_handler)
-
-def err(out):
-    print("ERROR: " + out)
-    exit(-1)
 
 def hook_name(name):
     """Return function name of a hook point to attach"""
@@ -329,8 +327,16 @@ def print_event(cpu, data, size):
                 sym = bpf.ksym(addr, show_offset=True)
                 print("\t%s" % sym)
 
+def apply_cpu_list(bpf, cpu_list):
+    """Apply the cpu_list to BPF program"""
+    cpu_array = bpf.get_table("trace_enabled")
+    out = 0
+    for cpu in cpu_list:
+        out |= 1 << cpu
+    cpu_array[0] = ctypes.c_uint64(out)
+
 def main():
-    global bpf, stack_traces, cpu_list
+    global bpf, stack_traces, cpu_list, body
 
     # Enable enabled tracepoints
     for name, entry in tracepoint_list.items():
@@ -341,7 +347,9 @@ def main():
         if not entry["enabled"]:
             continue
         hook_append(name, _type="kprobe", _sub_type=entry["subtype"])
-    body.replace("GENERATED_HOOKS", hooks)
+    body = body.replace("GENERATED_HOOKS", hooks)
+    body = body.replace("BACKTRACE_ENABLED", "1" if args.backtrace else "0")
+    body = body.replace("MAX_N_CPUS", "%d" % MAX_N_CPUS)
     if args.debug:
         print(body)
         exit(0)
@@ -362,10 +370,9 @@ def main():
             bpf.attach_kprobe(event=kprobe, fn_name=hook_name(name))
         print("Enabled hook point: %s" % name)
 
-    stack_traces = bpf.get_table("stack_traces")
-    cpu_array = bpf.get_table("trace_enabled")
-    for cpu in cpu_list:
-        cpu_array[cpu] = ctypes.c_uint64(1)
+    if args.backtrace:
+        stack_traces = bpf.get_table("stack_traces")
+    apply_cpu_list(bpf, cpu_list)
 
     print("%-18s %-20s %-4s %-8s %s" % ("TIME(s)", "COMM", "CPU", "PID", "TYPE"))
     bpf["events"].open_perf_buffer(print_event)
