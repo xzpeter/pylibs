ef925465dee57c14a2590d0d3e47b5dbe9c878ee
Peter Xu <peterx@redhat.com>
2021-03-26 13:27:43 -0400

    update version to v0.1.1
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- rt-trace-bcc.py
+++ rt-trace-bcc.py
@@ -39,7 +39,7 @@ import ctypes
 import sys
 import os
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # Limit this because we use one u64 as cpumask.  Problem is BPF does not allow
 # loop, so any real cpumask won't work.
